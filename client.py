e128bdaafcf8c47456e9f5dd87e3622b6604dd81
VelizAndres <dandresveliz18@gmail.com>
2022-07-01 19:08:55 -0600

    Agregar Logica de Juego

--- client.py
+++ client.py
@@ -1,8 +1,10 @@
 # RODOLFO BACILIO CHIVALAN DE LEON     1561817
 # DIEGO ANDRES VELIZ                   1230019
+import random
 
 from fighter import Fighter
 from jugador import Jugador
+import random
 
 # Importacion de Personajes
 from superman import Superman
@@ -41,10 +43,11 @@ class Cliente:
             player2.show_statistics()
             print("-".center(40, "-"))
             print('\nComenzando el duelo...')
-            
-
-
-
+            print('\n\n')
+            if player1.get_personaje().get_speed() >= player2.get_personaje().get_speed():
+                self.duelo(player1, player2)
+            else:
+                self.duelo(player2, player1)
 
     def elegir_personaje(self, num: int) -> Jugador:
         print("_".center(40, "_"))
@@ -146,6 +149,67 @@ class Cliente:
                 continue
         return player
 
+    def duelo(self, luchador1: Jugador, luchador2: Jugador):
+        prob1: int = 0
+        prob2: int = 0
+        while prob1 != -1 and prob2 != -1:
+            prob1 = self.ataques(luchador1, luchador2, prob1)
+            print("-".center(40, "-"))
+            print(f'{luchador1.get_name()} > Vida= {luchador1.get_personaje().get_hp()}'.center(40, "-"))
+            print(f'{luchador2.get_name()} > Vida= {luchador2.get_personaje().get_hp()}'.center(40, "-"))
+            print("-".center(40, "-"))
+            prob2 = self.ataques(luchador2, luchador1, prob2)
+            print("-".center(40, "-"))
+            print(f'{luchador1.get_name()} > Vida= {luchador1.get_personaje().get_hp()}'.center(40))
+            print(f'{luchador2.get_name()} > Vida= {luchador2.get_personaje().get_hp()}'.center(40))
+            print("-".center(40, "-"))
 
+    def ataques(self, atacante: Jugador, atacado: Jugador, probabilidad: int) -> int:
+        option: int = 0
+        while option != 4:
+            print("-".center(40, "-"))
+            print(f'Jugador: {atacante.get_name()}')
+            print('Escoja entre los siguientes ataques')
+            print('1- Ataque Normal')
+            if probabilidad < 4:
+                print(f'2- Recargar super Ataque (Probabilidad: {probabilidad*25} %)')
+            if probabilidad > 0:
+                print('3- Super Ataque')
+            print("-".center(40, "-"))
+            option = int(input('Ingrese un Ataque  >> '))
+            if option == 1:
+                print(f'Ha elegido ataque normal')
+                probabilidad = self.Damage(atacante, atacado, False)
+                option = 4
+            elif option == 2:
+                print(f'Ha elegido Recargar Super Ataque')
+                probabilidad = probabilidad + 1
+                option = 4
+            elif option == 3:
+                print(f'Ha elegido Super Ataque')
+                rand: int = random.randint(1, 4)
+                if probabilidad >= rand:
+                    probabilidad = self.Damage(atacante, atacado, True)
+                else:
+                    print(f'El Super Ataque ha Fallado')
+                    probabilidad = 0
+                option = 4
+            else:
+                print('Ingrese una opcion correcta')
+                continue
+        return probabilidad
+
+    def Damage(self, p1: Jugador, p2: Jugador, critico: bool) -> int:
+        damage: float = p2.get_personaje().compute_damage(p1.get_personaje())
+        if critico:
+            damage = damage * 1.5
+            print(f'Daño Critico Activado')
+        print(f'Daño Provacado {damage}')
+        p2.get_personaje().reduce_hp(damage)
+        if p2.get_personaje().get_hp() <= 0:
+            print(f'El ponente {p2.get_name()} ha perdido')
+            return -1
+        else:
+            return 0
 main = Cliente()
 main.pantalla()
